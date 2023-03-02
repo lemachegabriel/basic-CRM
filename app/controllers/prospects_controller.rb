b95bbd683c58a16febce40d342849debaf74ae91
lemachegabriel <lemache35@gmail.com>
2023-03-02 18:00:10 -0300

    added create prospect page

--- app/controllers/prospects_controller.rb
+++ app/controllers/prospects_controller.rb
@@ -1,6 +1,6 @@
 class ProspectsController < ApplicationController
   PROSPECTS_PER_PAGE = 20
-  
+
   def index
     @table_fields = ["First Name", "Last Name", "Email", "Stage", "Phone", "Company", "Probability", "Edit", "Delete"]
 
@@ -11,6 +11,22 @@ class ProspectsController < ApplicationController
     @total_pages = (@total_prospects.to_f / PROSPECTS_PER_PAGE).ceil
 
     @prospects = Prospect.limit(PROSPECTS_PER_PAGE).offset((@current_page - 1) * PROSPECTS_PER_PAGE)
+  end
+
+  def create
+    if Prospect.create(prospect_params)
+      flash[:success] = "Client created successfully."
+      redirect_to request.referrer
+    else
+      flash.now[:error] = "Could not create."
+      redirect_to request.referrer
+    end
+  end
+
+  def new
+    @prospect = Prospect.all
+    @stages = Stage.all
+    @companies = Company.order('name ASC')
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-
+  get 'prospects', to: 'prospects#index'
   get 'prospects/:page', to: 'prospects#index'
   get 'prospects/edit/:id', to: 'prospects#edit'
+  post 'prospects/create/', to: 'prospects#create'
   post 'prospects/edit/:id', to: 'prospects#update'
   delete 'prospects/:id', to: 'prospects#destroy'
+  get 'prospect/new', to: 'prospects#new'
   
   get 'company/:id', to: 'companies#index'
   get 'company/edit/:id', to: 'companies#edit'
